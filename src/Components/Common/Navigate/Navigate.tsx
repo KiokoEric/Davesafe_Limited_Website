14a9f10d20a907cc9251b890a2e41100a5480812
KiokoEric <kiokoerick040@gmail.com>
2025-04-30 17:36:31 +0300

    Addition of image source to the reusable Navigate component.

--- src/Components/Common/Navigate/Navigate.tsx
+++ src/Components/Common/Navigate/Navigate.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 interface NavigateProps {
+    Image?: any;
     Navigation?: any;
     NavigateText: string;
     NavigateStyle: string;
 }
 
-const Navigate: React.FC<NavigateProps> = ({ Navigation, NavigateStyle, NavigateText }) => {
+const Navigate: React.FC<NavigateProps> = ({ Navigation, NavigateStyle, Image, NavigateText }) => {
 return (
     <Link to={Navigation} className={NavigateStyle}>
+        <img src={Image} alt="" />
         { NavigateText }
     </Link>
 )
